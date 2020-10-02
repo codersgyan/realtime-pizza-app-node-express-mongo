fb6d5968657aab343027555ee7bdd980d0518d27
Rakesh K <codersgyan@gmail.com>
2020-10-02 23:57:06 +0300

    add semicolon

--- server.js
+++ server.js
@@ -22,38 +22,38 @@ connection.once('open', () => {
     console.log('Connection failed...')
 });
 
-// Session store 
+// Session store
 let mongoStore = new MongoDbStore({
                 mongooseConnection: connection,
                 collection: 'sessions'
             })
 
-// Event emitter 
+// Event emitter
 const eventEmitter = new Emitter()
 app.set('eventEmitter', eventEmitter)
 
 // Session config
 app.use(session({
     secret: process.env.COOKIE_SECRET,
-    resave: false, 
+    resave: false,
     store: mongoStore,
-    saveUninitialized: false, 
-    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour 
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
 }))
 
-// Passport config 
+// Passport config
 const passportInit = require('./app/config/passport')
 passportInit(passport)
 app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(flash())
-// Assets 
+// Assets
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-// Global middleware 
+// Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
     res.locals.user = req.user
@@ -67,14 +67,14 @@ app.set('view engine', 'ejs')
 require('./routes/web')(app)
 
 const server = app.listen(PORT , () => {
-            console.log(`Listening on port ${PORT}`)
+            console.log(`Listening on port ${PORT}`);
         })
 
-// Socket 
+// Socket
 
 const io = require('socket.io')(server)
 io.on('connection', (socket) => {
-      // Join  
+      // Join
       socket.on('join', (orderId) => {
         socket.join(orderId)
       })
